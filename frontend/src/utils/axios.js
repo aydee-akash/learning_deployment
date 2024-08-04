526765408b2c2d98f842df70588a0bbfa5c2e8a1
aydee-akash <137648244+aydee-akash@users.noreply.github.com>
2024-08-04 19:21:14 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: 'https://todos-backend-eight.vercel.app',
+    baseURL: 'https://vercel.com/akashs-projects-1dab1190/learning-deployment/syngegyp7cJFYgVLPqxqxbz3HXxY',
     // baseURL: 'http://localhost:4444',
     withCredentials: true
 });
