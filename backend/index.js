6326826751b3cf2c72800b2a276ad794bcf59de8
aydee-akash <137648244+aydee-akash@users.noreply.github.com>
2024-08-04 19:31:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 4444;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "4kb" }));
 app.use(cors({
-    origin: ["https://todos-frontend-fawn.vercel.app"],
+    origin: ["https://learning-deployment-frontend.vercel.app"],
     credentials: true
 }))
 
